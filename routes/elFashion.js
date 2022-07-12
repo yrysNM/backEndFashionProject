b56e9cfd133a2b67aa6a97deb0dc4896a1af41e1
yrys_nm <yrys.n3m@gmail.com>
2022-07-12 13:08:12 +0600

    new db

--- routes/elFashion.js
+++ routes/elFashion.js
@@ -2,11 +2,11 @@ const express = require("express");
 const dbo = require("../db/conn");
 const ObjectId = require("mongodb").ObjectId;
 
-const orderRoutes = express.Router();
-orderRoutes.route("/order").get(function (req, res) {
+const elFashionRoutes = express.Router();
+elFashionRoutes.route("/listProviders").get(function (req, res) {
     let db_connect = dbo.getDbElFashion("elFashion");
     db_connect
-        .collection("order")
+        .collection("provider")
         .find({})
         .limit(10)
         .toArray((err, result) => {
@@ -15,5 +15,31 @@ orderRoutes.route("/order").get(function (req, res) {
         });
 });
 
+elFashionRoutes.route("/provider").post( async (req, res) => {
+    const {name, phone, email, provider} = req.body;
+    const db_connect = dbo.getDbElFashion("elFashion");
+    const collection = db_connect.collection("provider");
 
-module.exports = orderRoutes;
+
+    
+    const response = await collection.insertOne({
+            name, 
+            phone, 
+            email,
+            provider
+        });
+    
+    const result = {
+        _id: response.insertedId, 
+        name, 
+        phone, 
+        email,
+        provider 
+    };
+
+    res.json(result);
+
+});
+
+
+module.exports = elFashionRoutes;
