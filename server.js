0c6f75dcdc775efb31a7a59c0c6615104b132ee4
yrys_nm <yrys.n3m@gmail.com>
2022-07-15 11:16:22 +0600

    post method

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require("express"); 
+const express = require("express");
 const cors = require("cors");
-const dbo = require("./db/conn"); 
-require("dotenv").config({path: "./config.env"});
+const dbo = require("./db/conn");
+require("dotenv").config({ path: "./config.env" });
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,10 +10,9 @@ app.use(express.json());
 app.use(require("./routes/products"));
 app.use(require("./routes/elFashion"));
 
-
 app.listen(port, () => {
-    dbo.connectToServer(function(err) {
-        if(err) console.error(err);
-    });
-    console.log(`Server is running on port ${port}`);
-})
+  dbo.connectToServer(function (err) {
+    if (err) console.error(err);
+  });
+  console.log(`Server is running on port ${port}`);
+});
