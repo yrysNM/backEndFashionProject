4699dfd646277028fd8a0721df5c0d878f8e9539
yrys_nm <yrys.n3m@gmail.com>
2022-07-19 02:01:23 +0600

    change request

--- routes/elFashion.js
+++ routes/elFashion.js
@@ -4,41 +4,38 @@ const ObjectId = require("mongodb").ObjectId;
 
 const elFashionRoutes = express.Router();
 elFashionRoutes.route("/listProviders").get(function (req, res) {
-    let db_connect = dbo.getDbElFashion("elFashion");
-    db_connect
-        .collection("provider")
-        .find({})
-        .limit(10)
-        .toArray((err, result) => {
-            if (err) throw err;
-            res.json(result);
-        });
+  let db_connect = dbo.getDbElFashion("elFashion");
+  db_connect
+    .collection("provider")
+    .find({})
+    .limit(10)
+    .toArray((err, result) => {
+      if (err) throw err;
+      res.json(result);
+    });
 });
 
-elFashionRoutes.route("/provider").post( async (req, res) => {
-    const {name, phone, email, provider} = req.body;
-    const db_connect = dbo.getDbElFashion("elFashion");
-    const collection = db_connect.collection("provider");
+elFashionRoutes.route("/provider").post(async (req, res) => {
+  const { name, phone, email, provider } = req.body;
+  const db_connect = dbo.getDbElFashion("elFashion");
+  const collection = db_connect.collection("provider");
 
+  const response = await collection.insertOne({
+    name,
+    phone,
+    email,
+    provider,
+  });
 
-    
-    const response = await collection.insertOne({
-            name, 
-            phone, 
-            email,
-            provider
-        });
-    
-    const result = {
-        _id: response.insertedId, 
-        name, 
-        phone, 
-        email,
-        provider 
-    };
+  const result = {
+    _id: response.insertedId,
+    name,
+    phone,
+    email,
+    provider,
+  };
 
-    res.json(result);
-
+  res.json(result);
 });
 
 
