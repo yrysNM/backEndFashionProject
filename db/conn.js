c9bca5ebab23ba6eb39a75d18599451e3260e1bc
yrys_nm <yrys.n3m@gmail.com>
2022-07-20 19:58:00 +0600

    upload img

--- db/conn.js
+++ db/conn.js
@@ -1,18 +1,18 @@
 const { MongoClient } = require("mongodb");
-require("dotenv").config({path: "./config.env"});
-const Db= process.env.ATLAS_URI;
+require("dotenv").config({ path: "./config.env" });
+const Db = process.env.ATLAS_URI;
 const client = new MongoClient(Db, {
-    useNewUrlParser: true, 
+    useNewUrlParser: true,
     useUnifiedTopology: true,
-}); 
+});
 
-var _db; 
-
+var _db;
+var _dbelFashion;
 module.exports = {
-    connectToServer:  function(callback) {
-        client.connect(function(err, db) {
-            if(db) {
-                _db = db.db("products"); 
+    connectToServer: function (callback) {
+        client.connect(function (err, db) {
+            if (db) {
+                _db = db.db("products");
                 _dbelFashion = db.db("elFashion");
                 console.log("Successfully, connected to MongoDB");
             }
@@ -20,7 +20,7 @@ module.exports = {
         });
     },
 
-    getDb: function() {
+    getDb: function () {
         return _db;
     },
     getDbElFashion: () => {
--- routes/elFashion.js
+++ routes/elFashion.js
@@ -1,8 +1,48 @@
 const express = require("express");
 const dbo = require("../db/conn");
+const multer = require("multer");
+const fs = require("fs");
+const path = require("path");
 const ObjectId = require("mongodb").ObjectId;
 
 const elFashionRoutes = express.Router();
+
+
+const storage = multer.diskStorage({
+  destination: 'routes/uploads',
+  filename: (req, file, cb) => {
+    cb(null, file.originalname)
+  }
+});
+
+
+const upload = multer({ storage: storage });
+
+elFashionRoutes.route("/catalogs").post(upload.single("image"), async (req, res) => {
+  const db_connect = dbo.getDbElFashion("elFashion");
+  const collection = db_connect.collection("uploadedImages");
+
+  // console.log(req.file);
+  const response = await collection.insertOne({
+    title: req.body.title,
+    img: {
+      data: fs.readFileSync(path.join(__dirname + "/uploads/" + req.file.filename)),
+      contentType: req.body.type,
+
+    }
+  });
+
+  const result = {
+    _id: response.insertedId,
+    title: "test3",
+    img: response.img,
+  };
+  console.log(result)
+  res.json(result);
+});
+
+
+
 elFashionRoutes.route("/listProviders").get(function (req, res) {
   let db_connect = dbo.getDbElFashion("elFashion");
   db_connect
