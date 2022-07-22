57c9466c8b9e04a1c3c646ecc62beaab0ccae623
yrys_nm <yrys.n3m@gmail.com>
2022-07-22 21:11:08 +0600

    delte logs

--- routes/elFashion.js
+++ routes/elFashion.js
@@ -26,17 +26,17 @@ conn.once("open", () => {
 
 // media routes
 elFashionRoutes.route("/file/:filename").get(async (req, res) => {
-  // try {
-  // console.log(gfs);
-  const file = await gfs.files.findOne({ filename: req.params.filename });
-  // console.log(file)
-  const readStream = gridfsBucket.openDownloadStream(file._id);
-  readStream.pipe(res);
-  // res.send(file);
-  // } catch (error) {
-  //   console.log(error);
-  //   res.send("not found :(");
-  // }
+  try {
+
+    const file = await gfs.files.findOne({ filename: req.params.filename });
+
+    const readStream = gridfsBucket.openDownloadStream(file._id);
+    readStream.pipe(res);
+
+  } catch (error) {
+    console.log(error);
+    res.send("not found :(");
+  }
 });
 
 elFashionRoutes.route("/uploadImg").post(upload.single("image"), async (req, res) => {
