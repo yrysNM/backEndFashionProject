75ffc5246bb6138445af1dddcc5ea16cdec75165
yrys_nm <yrys.n3m@gmail.com>
2022-07-23 13:28:42 +0600

    cors

--- middleware/upload.js
+++ middleware/upload.js
@@ -6,7 +6,7 @@ const storage = new GridFsStorage({
     url: process.env.MONGODB_URI,
     options: { useNewUrlParser: true, useUnifiedTopology: true },
     file: (req, file) => {
-        const match = ["image/png", "image/jpeg"];
+        const match = ["image/png", "image/jpeg", "image/jpg", "image/jpe", "image/webp", "image/gif"];
 
         if (match.indexOf(file.mimetype) === -1) {
             const filename = `${Date.now()}-any-name-${file.originalname}`;
